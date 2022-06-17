016557daac83968bb19589767cb1db6564604f36
Stefan <stefan.sm2411@gmail.com>
2022-06-17 14:44:56 +0200

    Navbar izmene

--- single.php
+++ single.php
@@ -72,25 +72,27 @@
         <div class="container position-relative" style="z-index: 9;">
             <nav class="navbar navbar-expand-lg bg-secondary navbar-dark py-3 py-lg-0 pl-3 pl-lg-5">
                 <a href="" class="navbar-brand">
-                    <h1 class="m-0 display-5 text-white"><span class="text-primary">i</span>DESIGN</h1>
+                    <h1 class="m-0 display-5 text-white"><span class="text-primary">iznajmi</span>STAN</h1>
                 </a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
                     <div class="navbar-nav ml-auto py-0">
-                        <a href="index.html" class="nav-item nav-link">Home</a>
-                        <a href="about.html" class="nav-item nav-link">About</a>
-                        <a href="service.html" class="nav-item nav-link">Service</a>
-                        <a href="project.html" class="nav-item nav-link">Project</a>
+                        <a href="index.php" class="nav-item nav-link active">Početna</a>
+                        <a href="service.php" class="nav-item nav-link">Usluge</a>
+                        <a href="project.php" class="nav-item nav-link">Projekti</a>
                         <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle active" data-toggle="dropdown">Pages</a>
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
                             <div class="dropdown-menu rounded-0 m-0">
                                 <a href="blog.html" class="dropdown-item">Blog Grid</a>
                                 <a href="single.html" class="dropdown-item">Blog Detail</a>
                             </div>
                         </div>
-                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+                        <a href="contact.php" class="nav-item nav-link">Kontakt</a>
+                        <a href="about.php" class="nav-item nav-link">O nama</a>
+                        <a href="login.php" class="nav-item nav-link">Log in</a>
+                        <a href="signup.php" class="nav-item nav-link">Sign up</a>
                     </div>
                 </div>
             </nav>
